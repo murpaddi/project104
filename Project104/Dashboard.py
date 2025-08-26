bb6c35cacb30807140badf3b2ede40a178d06c82
Angus <angus.j.murphy@outlook.com>
2025-08-26 20:20:51 +1000

    Delete Project104 directory

--- Project104/Dashboard.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def show_dashboard():
-    with st.container():
-        st.title("Marybyrnong Smart City Bins Dashboard - Project 104")
-        st.divider()
-        st.markdown("This is the dashboard page")
